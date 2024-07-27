6fc9835c122f01486408c787fca7afc215994d75
owarewaa <akosuaowarewaa360@gmail.com>
2024-07-27 17:12:18 +0000

    Added firstname and lastname to validator

--- validators/user_validator.js
+++ validators/user_validator.js
@@ -1,8 +1,12 @@
 import Joi from "joi";
 
 export const userSchema = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    otherNames: Joi.string(),
     username: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(4).required(),
-    
-})
+    confirmPassword: Joi.ref('password'),
+
+}).with('password', 'confirmPassword')
