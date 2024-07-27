73d11814c22cbf3bd4ec8119a18999ddb755c32b
owarewaa <akosuaowarewaa360@gmail.com>
2024-07-27 19:05:49 +0000

    changes

--- models/user_model.js
+++ models/user_model.js
@@ -17,12 +17,3 @@ const userSchema = new mongoose.Schema({
 userSchema.plugin(toJSON);
 export const UserModel = model("User", userSchema);
 
-const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  });
-
-  userSchema.plugin(toJSON);
-  export const UserModel = model("User", userSchema);
-
