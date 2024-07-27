4761a9e402291ed1f01e3903fc3435b1a3d22e85
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-07-27 15:58:16 +0000

    Updated routes with logout

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import { login, signup } from "../controllers/user_controller.js"; 
+import {signup, login, logout  } from "../controllers/user_controller.js"; 
 
 
 export const userRouter = Router();
 
+userRouter.post("/users/auth/signup", signup);
 
 userRouter.post("/users/auth/token/login", login);
 
-userRouter.post("/users/auth/signup", signup);
-
+userRouter.post("users/auth/logout", logout)
