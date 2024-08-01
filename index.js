7b6156e2d1ffef05304953d21a8a2cf7dadae8af
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-08-01 11:13:27 +0000

    swagger

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ import mongoose from "mongoose"
 import { dbConnection } from "./config/db.js";
 import { userRouter } from "./routes/user_routes.js";
 import { favouriteRouter } from "./routes/favourite_routes.js";
+import expressOasGenerator from "express-oas-generator"
 
 const app = express();
 
+
 dbConnection();
+
+expressOasGenerator.handleResponses(app,{
+    alwaysServeDocs: true,
+    tags: ["auth","favourites"],
+    mongooseModels: mongoose.modelNames(),
+});
+    
 
 
 
@@ -14,8 +23,11 @@ app.use(express.json());
 
 app.use(userRouter);
  app.use(favouriteRouter);
+expressOasGenerator.handleRequests();
+app.use((req,res) => res.redirect("/api-docs"));
 
 
 app.listen(4500, () =>
     console.log('Server is running') 
 )
+
