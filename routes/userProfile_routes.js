d45c166c651559d4f6558eda3bbfb22047d2c643
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-08-02 16:43:46 +0000

    UserProfileRouterNew

--- routes/userProfile_routes.js
+++ routes/userProfile_routes.js
@@ -1,17 +1,15 @@
 import { Router } from "express";
 import {createUserProfile,updateUserProfile,getUserProfile }from "../controllers/userProfile_controller.js";
-import authenticate from "../middlewares/auth.js"
+import { isAuthenticated } from "../middlewares/auth.js";
+
 
 
 export const userProfileRouter = Router();
 
-// Route to create a new user profile
-userProfileRouter.post('/', authenticate, createUserProfile);
+userProfileRouter.post('/create', isAuthenticated , createUserProfile);
 
-// Route to update an existing user profile
-userProfileRouter.put('/:id', authenticate, updateUserProfile);
+userProfileRouter.put('/update/:id', isAuthenticated , updateUserProfile);
 
-// Route to get a user's profile
-userProfileRouter.get('/', authenticate, getUserProfile);
+userProfileRouter.get('/profile', isAuthenticated , getUserProfile);
 
 export default userProfileRouter;
