873e8a9435503b2ff4dab78b1c1aecce50647c4e
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-08-02 10:34:02 +0000

    new

--- routes/getUser_route.js
+++ routes/getUser_route.js
@@ -5,6 +5,5 @@ export const getUserRouter = Router();
 
 
 getUserRouter.get("/users", getAllUsers);
-// getUserRouter.post("/users/auth/forgotPassword", forgotPassword);
 getUserRouter.post('/forgot-password/send-otp', sendOtpForPasswordReset);
 getUserRouter.post('/forgot-password/verify-otp', verifyOtpAndResetPassword);
